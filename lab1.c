fa0065e5e8b64ec63468439ca7430af970f46819
cj meeks <cj meeks>
2016-02-03 13:36:18 -0600

    second

--- lab1.c
+++ lab1.c
@@ -1,7 +1,22 @@
 #include <stdin.h>
+#include <ctype.h>
+#include <string.h>
 
-int int main(int argc, char const *argv[]) {
-  printf("Please enter a Key\n", );
-  
+int main(int argc, char const *argv[]) {
+  char[27] alphabet = {'ABCDEFGHIJKLMNOPQRSTUVWXYZ'};
+  char[] key;
+  printf("Please enter a Key: ", );
+  scanf("%s\n",&key );
+
+
   return 0;
+}
+char[] createAlphabetAtChar(char[] bet, int charAt){
+  char[27] new;
+  int count = 0;
+  for (size_t i = charAt; i != charAt-1; i++) {
+    if(i == 26) i=0;
+    &new[i] = &bet[count];
+        /* code */
+  }
 }
